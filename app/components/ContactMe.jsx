f9a6a43b48cb38591dad06c90bb072d5bff1aae9
1yavuz <1yavuzahmet@gmail.com>
2025-03-11 00:21:30 +0300

    re header name contact me and my work pages

--- app/components/ContactMe.jsx
+++ app/components/ContactMe.jsx
@@ -4,7 +4,10 @@ import error from "../../assets/page-error.gif"
 
 const ContactMe = () => {
     return (
-        <div id='contact' className='w-full px-[12%] scroll-mt-20 my-20 text-center'><Image src={error} alt='' /></div>
+        <div id='contact' className='w-full px-[12%] scroll-mt-20 my-20 text-center'>
+            <h4 className='text-center mb-2 text-lg'>Introduction</h4>
+            <h2 className='text-center text-5xl'>Contact Me</h2>
+            <Image src={error} alt='' /></div>
     )
 }
 
