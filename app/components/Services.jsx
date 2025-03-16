53237883ce2b53391464bfbb272413eb3da3e553
1yavuz <1yavuzahmet@gmail.com>
2025-03-16 18:02:37 +0300

    deleted prev not using data

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,9 +1,8 @@
-import { assets, projectsData, serviceData } from '@/assets/assets'
+import { projectsData } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
 import LinkIcon from '@mui/icons-material/Link';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Divider } from '@mui/material';
 
 const Services = () => {
     return (
@@ -13,19 +12,6 @@ const Services = () => {
             <h2 className='text-center text-5xl'>My Services</h2>
             <p className='text-center max-w-2xl mx-auto mt-5 mb-12'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt itaque fugiat velit possimus, vitae veritatis nihil provident molestiae neque voluptatum exercitationem eius expedita hic ex amet. Itaque atque ipsum odio!</p>
             <div className='grid  md:grid-cols-4 sm:grid-cols-2 gap-6 my-10'>
-                {/* {
-                    serviceData.map(({ icon, title, description, link }, index) => (
-                        <div key={index} className='text-justify 
-                        border border-gray-400 rounded-lg py-4 px-4 hover:bg-blue-100 hover:-translate-y-1 duration-300 cursor-pointer'>
-                            <Image src={icon} alt='' className=' w-10' />
-                            <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-                            <p className='text-xs text-gray-500 leading-5'>{description}</p>
-                            <a href={link}>
-                                read more <Image src={assets.right_arrow} alt='' className='w-4' />
-                            </a>
-                        </div>
-                    ))
-                } */}
                 {
                     projectsData.map(({ icon, github, link, title }, index) => (
                         <div key={index} className='text-justify 
