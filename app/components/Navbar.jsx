c903a7aba5270d333ab72abde1ac0bdc617074fb
1yavuz <1yavuzahmet@gmail.com>
2025-03-18 01:10:20 +0300

    moon icon deleted

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -44,10 +44,6 @@ const Navbar = () => {
                 </ul>
 
                 <div className='flex gap-2.5'>
-                    <button>
-                        <Image src={assets.moon_icon} alt='' className='w-6' />
-                    </button>
-
                     <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>
                         Contact
                         <Image src={assets.arrow_icon} className='w-3' alt='' />
--- assets/assets.js
+++ assets/assets.js
@@ -178,12 +178,8 @@ export const projectsData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
-];
-
-export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages and Teknology', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Management Information Systems (MIS)' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'You can find detailed projects in my work section.' }
 ];
 
